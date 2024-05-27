53d48676f9f8ded89866f20150a2528b906969f2
unknown <{apexa}+{apexapaudel484}@users.noreply.github.com>
2024-05-27 13:16:41 +0545

    changes

--- atm/utils/saving_video.py
+++ atm/utils/saving_video.py
@@ -1,7 +1,12 @@
-from fresh_frame import FreshestFrame
+from utils.fresh_frame import FreshestFrame
 import cv2
+import os
 
-def save_video(camera,output_name):
+def save_video(camera,output_folder,output_name):
+    os.makedirs(output_folder,exist_ok=True) 
+    print(output_folder,output_name)
+    output_name = os.path.join(output_folder,output_name)
+    # print
     cam = FreshestFrame(camera=camera , callback=None)
     fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
     out = cv2.VideoWriter(output_name, fourcc ,50, (640, 480)) 
